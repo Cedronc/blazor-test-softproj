0257c6dbbe0354ee09e0b97c39b4350095dd8082
Cedric Martel <s125461@ap.be>
2023-10-20 20:53:36 +0200

    added pokemon search function and show picture

---
+++ BlazorTestApp/Models/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlazorTestApp.Models;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+	public Sprites sprites { get; set; }
+	public int height { get; set; }
+	public int weight { get; set; }
+}
+
+public class Sprites
+{
+	public string back_default { get; set; }
+	public object back_female { get; set; }
+	public string back_shiny { get; set; }
+	public object back_shiny_female { get; set; }
+	public string front_default { get; set; }
+	public object front_female { get; set; }
+	public string front_shiny { get; set; }
+	public object front_shiny_female { get; set; }
+}
+
--- BlazorTestApp/Program.cs
+++ BlazorTestApp/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<CarRepository>();
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
